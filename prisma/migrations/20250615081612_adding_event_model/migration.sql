b1573bc2ee61847e6278ff43551c44ff4916c154
Matias Harding <matias.harding@gmail.com>
2025-06-15 05:13:30 -0400

    Prisma migrations for Event and User models

---
+++ prisma/migrations/20250615081612_adding_event_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
